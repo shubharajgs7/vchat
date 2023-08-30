92c8477a5274ad72150d8bc104953fee4eb16a26
Shubharaj <shubharajgs7@gmail.com>
2023-08-30 22:20:33 +0530

    changes

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,6 +1,5 @@
 //node server which will handle socket io connection
 const io = require("socket.io")(8000);
-
 const users = {};
 io.on("connection", socket => {
   socket.on("new-user-joined", myusername => {
